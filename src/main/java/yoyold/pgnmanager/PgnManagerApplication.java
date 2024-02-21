1d773f94fa3a0b0e945cbf70f0d30d1ea53521e6
yoyold <lukas.dony@web.de>
2024-02-21 11:12:51 +0100

    Add initial Spring Initializr files

---
+++ src/main/java/yoyold/pgnmanager/PgnManagerApplication.java
@@ -0,0 +1,13 @@
+package yoyold.pgnmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgnManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgnManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yoyold/pgnmanager/PgnManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package yoyold.pgnmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgnManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
